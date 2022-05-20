b45737df31ffc088df59c65aa72b6170a06f232c
tonyz840101 <tonyz840101@gmail.com>
2022-05-20 19:02:26 +0800

    update

--- V2/game.js
+++ V2/game.js
@@ -1,5 +1,5 @@
-class Game {
-    constructor(setting) {
+const Game =
+    function (setting) {
         /*
         {
             time,
@@ -9,136 +9,205 @@ class Game {
             scanSpeed,
         }
         */
-        this.time = setting.time || 180
+        /// rules
+        this.timeOption = setting.timeOption || [90, 180]
         this.tickPerSecode = setting.tickPerSecode || 40
         this.column = setting.column || 16
         this.row = setting.row || 10
         this.scanSpeed = setting.scanSpeed || this.tickPerSecode / 4 //default 4 blocks/sec
+        this.fallSpeed = setting.scanSpeed || this.tickPerSecode * 3 / 2 //1.5 sec
 
         this.innerColor = {
             C1: 1,
             C2: 2
         }
 
-        this.board = []
-        for (let r = 0; r < this.row; r++) {
-            this.board[r] = [];
-            for (let c = 0; c < this.column; c++) {
-                this.board[r][c] = false;
-            }
-        }
+        /// variables
+        this.pausing = false
+        this.currentGameTime = 0
+        this.state = gameState.menu
+        this.ticker = 0
         this.preList = []
-        for (let i = 0; i < 4; i++) {
-            this.preList[i] = this.generateBlockNumber()
-        }
         this.currentMovingBlock = {
             pattern: [],
-            x: 7
+            x: 7,
+            fall: 0,
+            fallTicker: 0
         }
 
-        this.state = gameState.idle
-        this.counter = 0
+        this.board = {
+            C1: [],
+            C2: []
+        }
+
+        for (let c = 0; c < this.column; c++) {
+            this.board.C1[c] = 0
+            this.board.C2[c] = 0
+        }
+
+
+
+        function generateBlockNumber() {
+            return ~~(Math.random() * 16)
+        }
+        for (let i = 0; i < 4; i++) {
+            this.preList[i] = generateBlockNumber()
+        }
 
         this.keyHandler = {
-            32: this.inputStart,
+            32: (v) => this.inputStart(v),
 
-            87: this.inputUp,//W
-            83: this.inputDown,//S
-            65: this.inputLeft,//A
-            68: this.inputRight,//D
-            38: this.inputUp,//A_UP
-            40: this.inputDown,//A_DOWN
-            37: this.inputLeft,//A_LEFT
-            39: this.inputRight,//A_RIGHT
+            87: (v) => this.inputUp(v),//W
+            83: (v) => this.inputDown(v),//S
+            65: (v) => this.inputLeft(v),//A
+            68: (v) => this.inputRight(v),//D
+            38: (v) => this.inputUp(v),//A_UP
+            40: (v) => this.inputDown(v),//A_DOWN
+            37: (v) => this.inputLeft(v),//A_LEFT
+            39: (v) => this.inputRight(v),//A_RIGHT
 
-            79: this.inputEnd,//end game
-            80: this.inputPause,//pause
+            79: (v) => this.inputEnd(v),//end game
+            80: (v) => this.inputPause(v),//pause
             // Z: 90:this.inputUp,
         }
 
-        console.log(this.keyHandler)
-    }
+        console.log(this.tickPerSecode)
+        this.loop = setInterval(() => this.gameLoop(), this.tickPerSecode)
 
-    generateBlockNumber() {
-        return ~~(Math.random() * 16)
-    }
 
-    decodeBlockNumber(v) {
-        let result = []
-        for (let i = 0; i < 4; i++) {
-            result[i] = ((v % 2) == 1) ? innerColor.C2 : innerColor.C1
-            tmp = tmp >> 1
+        this.gameTime = () => {
+            return this.timeOption[this.currentGameTime]
         }
-        return result
-    }
 
+        this.generateBlockNumber = generateBlockNumber
 
-    gameLoop() {
-        switch (this.state) {
-            case gameState.idle:
-                console.error('why are we still here')
-                break
-            case gameState.countDown:
-                this.state = this.countDownState()
-                break
-            case gameState.started:
-                this.state = this.startedState()
-                break
-            case gameState.result:
-                this.state = this.resultState()
-                break
+        this.decodeBlockNumber = (v) => {
+            let result = []
+            for (let i = 0; i < 4; i++) {
+                result[i] = ((v % 2) == 1) ? innerColor.C2 : innerColor.C1
+                tmp = tmp >> 1
+            }
+            return result
         }
-    }
 
-    countDownState() { }
-    startedState() { }
-    resultState() { }
 
-    handleKeyUp(e) {
-        const keyCode = e.keyCode
-        if (this.keyHandler[keyCode]) {
-            this.keyHandler[keyCode](true)
+
+        this.gameLoop = () => {
+
+            console.log(this.state, this.ticker)
+            switch (this.state) {
+                case gameState.menu:
+                    this.state = this.menuState()
+                    break
+                case gameState.countDown:
+                    this.state = this.countDownState()
+                    break
+                case gameState.started:
+                    this.state = this.startedState()
+                    break
+                case gameState.result:
+                    this.state = this.resultState()
+                    break
+                default:
+                    console.error('unknown state')
+                    return
+            }
         }
-    }
 
-    handleKeyDown(e) {
-        const keyCode = e.keyCode
-        if (this.keyHandler[keyCode]) {
-            this.keyHandler[keyCode](false)
+        this.menuState = () => {
+            return this.state
         }
-    }
+        this.countDownState = () => {
+            if (this.ticker > 0) {
+                this.ticker--
+                return this.state
+            } else {
+                this.ticker = this.gameTime() * this.tickPerSecode
+                return gameState.started
+            }
+        }
+        this.startedState = () => {
+            if (this.ticker > 0) {
+                this.ticker--
+                return this.state
+            } else {
+                return gameState.result
+            }
+        }
+        this.resultState = () => {
+            return this.state
+        }
 
-    inputStart(up) {
-        console.log('start', up)
-    }
-    inputUp(up) {
-        console.log('up', up)
-    }
-    inputDown(up) {
-        console.log('down', up)
-    }
-    inputLeft(up) {
-        console.log('left', up)
-    }
-    inputRight(up) {
-        console.log('right', up)
-    }
-    inputEnd(up) {
-        console.log('end', up)
-    }
-    inputPause(up) {
-        console.log('pause', up)
-    }
+        this.handleKeyUp = (e) => {
+            const keyCode = e.keyCode
+            if (this.keyHandler[keyCode]) {
+                this.keyHandler[keyCode](true)
+            }
+        }
 
-    start() {
-        this.loop = setInterval(this.gameLoop, this.tickPerSecode)
-        this.state = gameState.countDown
-        this.counter = this.tickPerSecode * 3
+        this.handleKeyDown = (e) => {
+            const keyCode = e.keyCode
+            if (this.keyHandler[keyCode]) {
+                this.keyHandler[keyCode](false)
+            }
+        }
+
+        this.inputStart = (up) => {
+            console.log('start', up)
+            if (!up) return
+            switch (this.state) {
+                case gameState.menu:
+                    this.state = gameState.countDown
+                    this.ticker = 3 * this.tickPerSecode
+                    return
+            }
+        }
+        this.inputUp = (up) => {
+            console.log('up', up)
+            if (up) return
+            switch (this.state) {
+                case gameState.menu:
+                    this.currentGameTime--
+                    if (this.currentGameTime < 0) {
+                        this.currentGameTime = this.timeOption.length - 1
+                    }
+                    console.log(this.gameTime())
+                    return
+            }
+        }
+        this.inputDown = (up) => {
+            console.log('down', up)
+            if (up) return
+            switch (this.state) {
+                case gameState.menu:
+                    this.currentGameTime++
+                    if (this.currentGameTime >= this.timeOption.length) {
+                        this.currentGameTime = 0
+                    }
+                    console.log(this.gameTime())
+                    return
+            }
+        }
+        this.inputLeft = (up) => {
+            console.log('left', up)
+        }
+        this.inputRight = (up) => {
+            console.log('right', up)
+        }
+        this.inputEnd = (up) => {
+            console.log('end', up)
+        }
+        this.inputPause = (up) => {
+            if (!up) return
+            console.log('pause', up)
+        }
+
+
+
     }
-}
 
 const gameState = {
-    idle: 0,
+    menu: 0,
     countDown: 1,
     started: 2,
     result: 3
