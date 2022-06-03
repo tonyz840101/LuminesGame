bba786e5c7ca4d10fa785c424feb494b7b757882
tonyz840101 <tonyz840101@gmail.com>
2022-06-03 18:38:43 +0800

    update color setting

--- V2/main.js
+++ V2/main.js
@@ -18,8 +18,8 @@ const KEYCODE = {
     ARROW_LEFT: 37,
     ARROW_RIGHT: 39,
 
-    K: 79,//end game
-    P: 80,//pause
+    K: 79, //end game
+    P: 80, //pause
     Z: 90,
 }
 const game = new Game({
@@ -28,19 +28,37 @@ const game = new Game({
 document.addEventListener("keydown", (e) => game.handleKeyDown(e), false);
 document.addEventListener("keyup", (e) => game.handleKeyUp(e), false);
 
+const c2 = ((colorHexStr) => {
+    let intColor = parseInt(colorHexStr.slice(1), 16)
+    if (isNaN(intColor)) {
+        console.error('C2 parseInt failed')
+    }
+    let ctR = intColor >> 16
+    let ctG = (intColor % (1 << 16)) >> 8
+    let ctB = intColor % (1 << 8)
+    let ct = `rgba(${ctR}, ${ctG}, ${ctB}, 0.5)`
+    console.log(colorHexStr, ct)
+    return {
+        o: colorHexStr,
+        t: ct
+    }
+
+})('#4A4AFF')
+// })('#0080FF')
 const colorProvider = new ObjColor({
     C1: '#cccccc',
     C1T: 'rgba(144, 144, 144, 0.5)',
     C1Shade: 'rgba(255, 255, 255, 0.9)',
-    C2: '#ff0000',
-    C2T: 'rgba(255, 0, 0, 0.5)',
+    C2: c2.o,
+    C2T: c2.t,
+    // C2: '#ff0000',
+    // C2T: 'rgba(255, 0, 0, 0.5)',
     C2Shade: 'rgba(0, 0, 0, 0.25)',
 })
 
 const renderer = new Renderer(
     document.getElementById("lowFreq"),
-    document.getElementById("highFreq"),
-    {
+    document.getElementById("highFreq"), {
         adjustX: 4,
         adjustY: 2,
         column: 16,
@@ -54,7 +72,13 @@ game.subscribe((e) => {
     switch (e.kind) {
         case game.eventKind.grouped:
             const time = 500
-            renderer.insertEffect({ kind: effectKind.grouped, x: e.x, y: game.row - e.y + 1, time, timeLeft: time })
+            renderer.insertEffect({
+                kind: effectKind.grouped,
+                x: e.x,
+                y: game.row - e.y + 1,
+                time,
+                timeLeft: time
+            })
             break
         case game.eventKind.cleared:
             break
